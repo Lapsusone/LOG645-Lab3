5394c30c148ae1b0b34b1970b77e588c01b64323
lapsusone <6044855+Lapsusone@users.noreply.github.com>
2019-07-09 14:50:18 -0400

    Other try

--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -87,7 +87,7 @@ void solvePar(int threads, int rows, int cols, int iterations, double td, double
           toSlaves[5] = r;
           toSlaves[6] = t;
           toSlaves[7] = b;
-          printf(threads);
+          cout << threads << endl;
           thread_rank = ++rank % (threads - 1);
 
           MPI_Send(&toSlaves, 8, MPI_DOUBLE, thread_rank, 1, MPI_COMM_WORLD);
