e190db5f595bbcf53c9e165763682870938f7758
lapsusone <6044855+Lapsusone@users.noreply.github.com>
2019-07-12 00:00:34 -0400

    ANother try

--- src/output/output.cpp
+++ src/output/output.cpp
@@ -13,24 +13,33 @@ using std::flush;
 using std::setprecision;
 using std::setw;
 
-void printMatrix(int rows, int cols, double ** matrix) {
-    for(int row = 0; row < rows; row++) {
-        for(int col = 0; col < cols; col++) {
+void printMatrix(int rows, int cols, double **matrix)
+{
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
             cout << fixed << setw(12) << setprecision(2) << matrix[row][col] << flush;
         }
 
-        cout << endl << flush;
+        cout << endl;
     }
 
-    cout << endl << flush;
+    cout << endl;
 }
 
-void printStatistics(int threads, long runtime_seq, long runtime_par) {
+void printStatistics(int threads, long runtime_seq, long runtime_par)
+{
     double acceleration = 1.0 * runtime_seq / runtime_par;
     double efficiency = acceleration / threads;
 
-    cout << endl << "Runtime sequential: " << runtime_seq / 1000000.0 << " seconds" << endl << flush;
-    cout << "Runtime parallel  : " << runtime_par / 1000000.0 << " seconds" << endl << flush;
-    cout << "Acceleration      : " << acceleration << endl << flush;
-    cout << "Efficiency        : " << efficiency << endl << flush;
+    cout << endl
+         << "Runtime sequential: " << runtime_seq / 1000000.0 << " seconds" << endl
+         << flush;
+    cout << "Runtime parallel  : " << runtime_par / 1000000.0 << " seconds" << endl
+         << flush;
+    cout << "Acceleration      : " << acceleration << endl
+         << flush;
+    cout << "Efficiency        : " << efficiency << endl
+         << flush;
 }
